71b222777d190d3763c9e79b741cd944c8c7ce25
SHIMA Ryosuke <shima19850604mail@gmail.com>
2019-10-07 17:10:19 +0900

    実行時エラーを修正

--- blockchain.py
+++ blockchain.py
@@ -8,7 +8,7 @@ from uuid import uuid4
 
 from flask import Flask, jsonify
 
-class Blockchain(object) :
+class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
@@ -16,7 +16,7 @@ class Blockchain(object) :
         # ジェネシスブロックを作る
         self.new_block(previous_hash=1, proof=100)
 
-    def new_block(self):
+    def new_block(self, proof, previous_hash=None):
         """
         ブロックチェーンに新しいブロックを作る
         :param proof: <int> プルーフ・オブ・ワークアルゴリズムから得られるプルーフ
@@ -97,56 +97,79 @@ class Blockchain(object) :
         """
 
         guess = f'{last_proof}{proof}'.encode()
-        guess_hash = hashlib_sha256(guess).hexdigest()
+        guess_hash = hashlib.sha256(guess).hexdigest()
 
         return guess_hash[:4] == "0000"
 
 
-    # ------------------------------------------------
-    # 以下、ローカルマシンにトランザクションエンドポイントを生成する処理
-    # ------------------------------------------------
+# ------------------------------------------------
+# 以下、ローカルマシンにトランザクションエンドポイントを生成する処理
+# ------------------------------------------------
 
-    # ノード作成
-    # Flaskの詳細 http://flask.pocoo.org/docs/0.12/quickstart/#a-minimal-application
-    app = Flask(__name__)
+# ノード作成
+# Flaskの詳細 http://flask.pocoo.org/docs/0.12/quickstart/#a-minimal-application
+app = Flask(__name__)
 
-    # このノードのグローバルにユニークなアドレスを作る
-    node_identifier = str(uuid4()).replace('-', '')
+# このノードのグローバルにユニークなアドレスを作る
+node_identifier = str(uuid4()).replace('-', '')
 
-    # ブロックチェーンクラスをインスタンス化する
-    blockchain = Blockchain()
+# ブロックチェーンクラスをインスタンス化する
+blockchain = Blockchain()
 
-    # メソッドはPOSTで/transactions/new エンドポイントを作る。POSTなので、データを送信する
-    @app.route('/transactions/new', methods=['POST'])
-    def new_transactions():
-        values = request.get_json()
+# メソッドはPOSTで/transactions/new エンドポイントを作る。POSTなので、データを送信する
+@app.route('/transactions/new', methods=['POST'])
+def new_transactions():
+    values = request.get_json()
 
-        # POSTされたデータのバリデーション
-        required = ['sender', 'recipient', 'amount']
-        if not all(k in values for k in required):
-            return 'Missing values', 400
+    # POSTされたデータのバリデーション
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
 
-        # 新しいトランザクション作成
-        index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+    # 新しいトランザクション作成
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
 
-        response = {'message': f'トランザクションはブロック{index}に追加されました'}
+    response = {'message': f'トランザクションはブロック{index}に追加されました'}
 
-        return jsonify(response), 201
+    return jsonify(response), 201
 
-    # メソッドはGETで/mineエンドポイントを作る
-    @app.route('/mine', methods=['GET'])
-    def mine():
-        return '新しいブロックを採掘します'
+# メソッドはGETで/mineエンドポイントを作る
+@app.route('/mine', methods=['GET'])
+def mine():
+    # 次のプルーフを見つけるためPoWアルゴリズムを使用する
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
 
-    # メソッドはGETで、フルのブロックチェーンをリターンする/chainエンドポイントを作る
-    @app.route('/chain', methods=['GET'])
-    def full_chain():
-        response = {
-            'chain': blockchain.chain,
-            'length': len(blockchain.chain),
-        }
-        return jsonify(response), 200
+    # プルーフ発見の報酬を得る
+    # 送信者は、採掘者が新しいコインを採掘したことを表すために"0"とする
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
 
-    # port5000でサーバーを起動する
-    if __name__ == '__main__':
-        app.run(host = '0.0.0.0', port = 5000)
+    # チェーンに新しいブロックを加えることで、新しいブロックを採掘
+    block = blockchain.new_block(proof)
+
+    response = {
+        'message': '新しいブロックを採掘しました',
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
+
+# メソッドはGETで、フルのブロックチェーンをリターンする/chainエンドポイントを作る
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+# port5000でサーバーを起動する
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port = 5000)
