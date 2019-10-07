a739b92cd7e32e84307e97eadd6802e720befae3
SHIMA Ryosuke <shima19850604mail@gmail.com>
2019-10-07 16:38:17 +0900

    トランザクションエンドポイント生成メソッドの処理を追加

--- blockchain.py
+++ blockchain.py
@@ -119,7 +119,19 @@ class Blockchain(object) :
     # メソッドはPOSTで/transactions/new エンドポイントを作る。POSTなので、データを送信する
     @app.route('/transactions/new', methods=['POST'])
     def new_transactions():
-        return '新しいトランザクションを追加します'
+        values = request.get_json()
+
+        # POSTされたデータのバリデーション
+        required = ['sender', 'recipient', 'amount']
+        if not all(k in values for k in required):
+            return 'Missing values', 400
+
+        # 新しいトランザクション作成
+        index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+        response = {'message': f'トランザクションはブロック{index}に追加されました'}
+
+        return jsonify(response), 201
 
     # メソッドはGETで/mineエンドポイントを作る
     @app.route('/mine', methods=['GET'])
