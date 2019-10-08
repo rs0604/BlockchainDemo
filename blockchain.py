a79656cac382edfcad9360b483b3ad5a65ed310d
SHIMA Ryosuke <shima19850604mail@gmail.com>
2019-10-08 10:25:23 +0900

    transaction/new で失敗する不具合を修正

--- blockchain.py
+++ blockchain.py
@@ -6,7 +6,7 @@ from textwrap import dedent
 from time import time
 from uuid import uuid4
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 class Blockchain(object):
     def __init__(self):
